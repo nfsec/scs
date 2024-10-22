5fec0ff3b5a4dadc0ce231c15508eae01d6bfb65
Patryk Krawaczynski <nfsec@users.noreply.github.com>
2024-10-22 21:46:36 +0200

    Unhider v0.2

--- README.md
+++ README.md
@@ -46,10 +46,26 @@
 
 ##### EXAMPLE:
 ```
-	root@darkstar:~# python bleachbitch.py
-	### Checking:  Links 2 - Web browser
-	~/.links2/links.his - Not Found.
-	~/.links/links.his - FOUND !!!
+    root@darkstar:~# python bleachbitch.py
+    ### Checking:  Links 2 - Web browser
+    ~/.links2/links.his - Not Found.
+    ~/.links/links.his - FOUND !!!
+```
+
+* **unhider** - *(Python 3.10.12)* Show hidden PIDs by libprocesshider.so on Linux system .
+
+##### EXAMPLE:
+```
+	root@darkstar:~# ./unhider.py
+	## Hidden PID revealer v0.2 by NFsec.pl
+	
+	Scanning system please wait...
+	
+	PIDs from /proc: [1, 1313, 1983, 1984, 3118, 3119, 3120, 3121, 3124, 3125]
+	
+	PIDs enumerated: [1, 3125, 3146, 3671, 3739, 3759, 3837, 3838, 3860, 3861]
+	
+	Hidden PIDs: [3860]
 ```
 
 ### License
---
+++ unhider/unhider.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+
+import os
+
+proc_path = '/proc'
+
+def process_list_behind_the_fog():
+    pid_list = []
+    for pid_number in os.listdir(proc_path):
+        if os.path.isdir(os.path.join(proc_path, pid_number)):
+            if pid_number.isnumeric():
+                pid_list.append(int(pid_number))
+    for pid_number in pid_list:
+        for task_number in os.listdir(os.path.join(proc_path, str(pid_number), "task")):
+            if int(task_number) in pid_list:
+                pass
+            else:
+                pid_list.append(int(task_number))
+    return(pid_list)
+
+def process_list_before_the_fog():
+    pid_list = []
+    with open('/proc/sys/kernel/pid_max', 'r') as pid_max:
+        upper_limit = int(pid_max.read()) + 1
+    for pid_number in range(0, upper_limit):
+        if os.path.isdir(os.path.join(proc_path, str(pid_number))):
+            pid_list.append(int(pid_number))
+    return(pid_list)
+
+if __name__ == "__main__":
+    print(f'## Hidden PID revealer v0.2 by NFsec.pl')
+    print(f'')
+    print(f'Scanning system please wait...')
+    print(f'')
+    print(f'PIDs from /proc: ' + str(process_list_behind_the_fog()))
+    print(f'')
+    print(f'PIDs enumerated: ' + str(process_list_before_the_fog()))
+    print(f'')
+    print(f'Hidden PIDs: ' + str(list(set(process_list_before_the_fog()) \
+                           - set(process_list_behind_the_fog()))))
