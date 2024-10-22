3fc5af0191b82dc0bf66358b88c8e11985f16961
Patryk Krawaczynski <nfsec@users.noreply.github.com>
2024-10-22 21:46:36 +0200

    Package Keeper - Python v3

--- pkgkeeper/pkgkeeper.py
+++ pkgkeeper/pkgkeeper.py
@@ -1,124 +1,104 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python3
 # -----------------
 # title            : PKGkeeper
-# description      : Script adds or removes packages from hold state 
-#                  : with apt-mark tool in Ubuntu [12.04.4 LTS] using sets.
+# description      : Script adds or removes packages from hold state
+#                  : with apt-mark tool in Ubuntu using python sets.
 # author           : Patryk 'agresor' Krawaczynski
-# date             : 25.08.2014
-# version          : 0.2
-# usage            : ./pkgkeeper.py package1 package2 package2
-#                  : ./pkgkeeper.py (clears all packages)
-# python version   : 2.7.8
-# required modules : python-apt 0.8.3 (https://launchpad.net/python-apt/)
+# date             : 25.03.2021
+# version          : 0.1
+# usage            : ./pkgkeeper package1 package2 package2
+#                  : ./pkgkeeper (clears all packages)
+# python version   : 3.X
+# required modules : python-apt (https://launchpad.net/python-apt/)
 # notes            : Script without arguments will purge all packages from list.
 # -----------------
 
-import gc
 import apt
 import sys
 import subprocess
 
-
 def error(message):
     """
-    Print error message and exit with status = 2.
-    :rtype : object
-    :param message:
+    Print error message and exit with status = 0.
     """
-    print message
-    sys.exit(2)
+    print(message)
+    sys.exit(1)
 
 
 def check_package(package):
     """
     Check if package is installed in system before marking it.
-    :type package: object
-    :param package: 
-    :return: 
     """
     try:
-        gc.collect()
         cache = apt.Cache()
         pkg = cache[package]
-        pkg.is_installed
         return pkg.is_installed
-    except KeyError, e:
+    except Exception as e:
         error("ERROR: " + str(e))
 
 
 def new_markers():
     """
     Read name of packages from scripts arguments and create set of them.
-    :return:
     """
-    new_markers = []
+    new_markers = set()
     for x in sys.argv[1:]:
         if check_package(x):
-            new_markers.append(x)
+            new_markers.add(x)
         else:
-            error("ERROR: Package %s not installed in system. Can't mark it." % x)
-    new_markers_set = set(new_markers)
-    return new_markers_set
+            error(f"ERROR: Package {x} not installed in system. Can't mark it.")
+    return new_markers
 
 
 def old_markers():
     """
     Read name of packages from existing 'hold list' and create set of them.
-    :rtype : object
-    :return:
     """
     try:
-        old_markers = subprocess.check_output("apt-mark showhold", stderr=subprocess.STDOUT, shell=True).splitlines()
-        old_markers_set = set(old_markers)
-        return old_markers_set
-    except subprocess.CalledProcessError, e:
+        showhold = (subprocess.check_output("apt-mark showhold", stderr=subprocess.STDOUT, shell=True, universal_newlines=True).splitlines())
+        old_markers = set(showhold)
+        return old_markers
+    except Exception as e:
         error("ERROR: " + str(e))
-
 
 def compare_markers(old, new):
     """
     Compare two sets and return information in list - what to add and what to remove.
-    :param old:
-    :param new:
-    :return:
     """
-    if new == old:
-        print "Nothing to add. Nothing to diminish. Carry on..."
-        sys.exit(0)
-    else:
-        add = new_markers().difference(old_markers())
-        add = list(add)
-        remove = old_markers().difference(new_markers())
-        remove = list(remove)
-        return add, remove
+    try:
+        if new == old:
+            sys.exit(0)
+        else:
+            add = new_markers().difference(old_markers())
+            add = list(add)
+            remove = old_markers().difference(new_markers())
+            remove = list(remove)
+            return add, remove
+    except Exception as e:
+        error("ERROR: " + str(e))
 
 
 def add_markers(*args):
     """
     Add new packages to hold state.
-    :param args:
-    :return:
     """
     if args is not None:
         try:
             for arg in args:
-                subprocess.check_output("apt-mark hold %s" % arg, stderr=subprocess.STDOUT, shell=True)
-        except subprocess.CalledProcessError, e:
+                subprocess.run(["apt-mark","hold",arg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except Exception as e:
             error("ERROR: " + str(e))
 
 
 def del_markers(*args):
     """
     Remove no longer needed packages from hold state.
-    :param args:
-    :return:
     """
     if args is not None:
         try:
             for arg in args:
-                subprocess.check_output("apt-mark unhold %s" % arg, stderr=subprocess.STDOUT, shell=True)
-        except subprocess.CalledProcessError, e:
+                subprocess.run(["apt-mark","unhold",arg], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        except Exception as e:
             error("ERROR: " + str(e))
 
 
