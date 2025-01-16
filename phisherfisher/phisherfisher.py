19957d0e07264d87b8815bda0a2cf3181e1a0a01
Patryk Krawaczyński <nfsec@users.noreply.github.com>
2025-01-16 09:31:20 +0100

    PhisherFisher v0.2

--- phisherfisher/phisherfisher.py
+++ phisherfisher/phisherfisher.py
@@ -47,7 +47,7 @@ def find_similar_score(entries: list[str]) -> bool:
         query=fqdn,
         choices=entries,
         limit=999999,
-        score_cutoff=80
+        score_cutoff=90
     )
     if not report:
         print(f'INFO: Based on score, no similar phishing domain(s) found.')
